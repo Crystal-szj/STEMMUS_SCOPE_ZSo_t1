11db604dc11b8aaf7c5c02cb5ee18567292235bc
Z.Song <z.song@utwente.nl>
2022-08-26 18:31:13 +0200

    Output root distribution

--- src/Initial_root_biomass.m
+++ src/Initial_root_biomass.m
@@ -1,4 +1,16 @@
-function [Rl] = Initial_root_biomass(RTB,DeltZ_R,rroot,ML)
+function [Rl, ri] = Initial_root_biomass(RTB,DeltZ_R,rroot,ML)
+% This function is used to calculate initial root length density (Rl) and
+% root distribution (ri).
+%{
+Input:
+    RTB:       A parameter of root to biomass [g m-2]
+    DeltZ_R:   An array of soil layer depth, from soil surface to bottom [m]
+    rroot:     A parameter of root radius [m]
+    ML:        Number of soil layer, same value with NL.
+Output:
+    Rl:        An array of root length density for each soil layer from bottom to surface [m m-3].
+    ri:        Root distribution: root fraction in each soil layers.
+%}
 global Ztot IGBP_veg_long
 %rri=zeros(1,45);
 root_den = 250*1000; %% [gDM / m^3] Root density  Jackson et al., 1997 
@@ -33,13 +45,14 @@ else
 end
 
 Rltot = RTB/R_C/root_den/(pi*(rroot^2)); %% %% root length index [m root / m^2 PFT]
-    %Ztot=DeltZ';
-   % Ztot=flip(Ztot);
-    Ztot=cumsum(DeltZ_R',1); 
-    %Ztot=flip(Ztot);
+
+% calculate the depth of each soil layer to land surface
+Ztot=cumsum(DeltZ_R',1); 
+
+% calculate root distribution
 for i=1:ML
     if i==1
-        ri(i)=(1-beta.^(Ztot(i)/2));
+        ri(i)=(1-beta.^(Ztot(i)/2)); % root fraction in each soil layer
     else
         ri(i)=beta.^(Ztot(i-1)-(DeltZ_R(i-1)/2))-beta.^(Ztot(i-1)+(DeltZ_R(i)/2));
     end
@@ -51,6 +64,8 @@ end
   %      rri(i)=(ri(i)+ri(i-1))./2;
  %   end
 %end
-Rl=(ri.*Rltot./(DeltZ_R./100))';
+Rl=(ri.*Rltot./(DeltZ_R./100))'; % root length density for each soil layer [m root / m^3 PFT]
+
+% filp root length density to the direction: bottom to surface
 Rl=flipud(Rl);
 end
