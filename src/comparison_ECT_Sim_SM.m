7acc26924445b1e5b0f699f8e44af8ccddfa7fba
Crystal-szj <z.song@utwente.nl>
2022-12-18 18:14:25 +0100

    Figure

--- src/comparison_ECT_Sim_SM.m
+++ src/comparison_ECT_Sim_SM.m
@@ -1,6 +1,6 @@
 % Compare ECT soil moisture and simulated soil moisture
 % read data
-obsDir = '../../CH_HTC_2022/Processed/Forcing_data_template_for_STEMMUS_SCOPE_CH-HTC_2022_0101_0930_v2.xlsx';
+obsDir = '../../CH_HTC_2022/Processed/Forcing_data_template_for_STEMMUS_SCOPE_CH-HTC_2022_0101_0809_v2.xlsx';
 obsSM = readtable(obsDir);
 
 % set simualtion depth of soil moisture
@@ -13,7 +13,7 @@ end
 % set simulated time 
 simSMTable = array2table(Sim_Theta,'VariableNames',variableNamesSM);
 startTime = datenum(datetime(2022,1,1,0,0,0));
-endTime = datenum(datetime(2022,5,1,23,59,0));
+endTime = datenum(datetime(2022,8,9,23,59,0));
 timeStep = datenum(datetime(2022,1,1,0,30,0)) - datenum(datetime(2022,1,1,0,0,0));
 datePeriod = (startTime: timeStep : endTime)';
 
@@ -21,19 +21,27 @@ simSMTable.dateTime = datetime(datePeriod,'ConvertFrom','datenum');
 simSMTable.SM_10cm = 0.5.*(simSMTable.SM_9cm+simSMTable.SM_11cm);
 simSMTable.SM_20cm = 0.5.*(simSMTable.SM_19cm+simSMTable.SM_21cm);
 %%
-f_drawSM(obsSM.dateTime, obsSM.Ms_5cm_Avg./100, simSMTable.dateTime, simSMTable.SM_5cm,  {'obs 5cm','sim 5cm'},  'Soil moisture(m^3/m^3)', [datetime(2022,1,1), datetime(2022,5,1)],[0.1,0.5],Output_dir,'ECT_sim_SM_5cm')
-f_drawSM(obsSM.dateTime, obsSM.Ms_10cm_Avg./100,simSMTable.dateTime, simSMTable.SM_10cm,  {'obs 10cm','sim 10cm'},  'Soil moisture(m^3/m^3)', [datetime(2022,1,1), datetime(2022,5,1)],[0.1,0.5],Output_dir,'ECT_sim_SM_10cm')
-f_drawSM(obsSM.dateTime, obsSM.Ms_20cm_Avg./100,simSMTable.dateTime, simSMTable.SM_20cm,  {'obs 20cm','sim 20cm'},  'Soil moisture(m^3/m^3)', [datetime(2022,1,1), datetime(2022,5,1)],[0.1,0.5],Output_dir,'ECT_sim_SM_20cm')
-f_drawSM(obsSM.dateTime, obsSM.Ms_40cm_Avg./100,simSMTable.dateTime, simSMTable.SM_40cm,  {'obs 40cm','sim 40cm'},  'Soil moisture(m^3/m^3)', [datetime(2022,1,1), datetime(2022,5,1)],[0.1,0.5],Output_dir,'ECT_sim_SM_40cm')
+f_drawSM(obsSM.dateTime, obsSM.Ms_5cm_Avg./100, simSMTable.dateTime, simSMTable.SM_5cm, obsSM.Precip, {'obs 5cm','sim 5cm','Precip'},  'Soil moisture(m^3/m^3)', [datetime(2022,1,1), datetime(2022,5,1)],[0.1,0.5],Output_dir,'ECT_sim_SM_5cm')
+f_drawSM(obsSM.dateTime, obsSM.Ms_10cm_Avg./100,simSMTable.dateTime, simSMTable.SM_10cm, obsSM.Precip,   {'obs 10cm','sim 10cm','Precip'},  'Soil moisture(m^3/m^3)', [datetime(2022,1,1), datetime(2022,5,1)],[0.1,0.5],Output_dir,'ECT_sim_SM_10cm')
+f_drawSM(obsSM.dateTime, obsSM.Ms_20cm_Avg./100,simSMTable.dateTime, simSMTable.SM_20cm,  obsSM.Precip,  {'obs 20cm','sim 20cm','Precip'},  'Soil moisture(m^3/m^3)', [datetime(2022,1,1), datetime(2022,5,1)],[0.1,0.5],Output_dir,'ECT_sim_SM_20cm')
+f_drawSM(obsSM.dateTime, obsSM.Ms_40cm_Avg./100,simSMTable.dateTime, simSMTable.SM_40cm,  obsSM.Precip,  {'obs 40cm','sim 40cm','Precip'},  'Soil moisture(m^3/m^3)', [datetime(2022,1,1), datetime(2022,5,1)],[0.1,0.5],Output_dir,'ECT_sim_SM_40cm')
+f_drawSM(obsSM.dateTime, obsSM.Ms_60cm_Avg./100,simSMTable.dateTime, simSMTable.SM_60cm,  obsSM.Precip,  {'obs 60cm','sim 60cm','Precip'},  'Soil moisture(m^3/m^3)', [datetime(2022,1,1), datetime(2022,5,1)],[0.1,0.5],Output_dir,'ECT_sim_SM_60cm')
+f_drawSM(obsSM.dateTime, obsSM.Ms_80cm_Avg./100,simSMTable.dateTime, simSMTable.SM_80cm,  obsSM.Precip,  {'obs 80cm','sim 80cm','Precip'},  'Soil moisture(m^3/m^3)', [datetime(2022,1,1), datetime(2022,5,1)],[0.1,0.5],Output_dir,'ECT_sim_SM_80cm')
 
-function f_drawSM(obsDateTime, obs, comDateTime, com, legendText, ylabelText,xlimRange, ylimRange, outputDir,outputName)
+function f_drawSM(obsDateTime, obs, comDateTime, com,precip, legendText, ylabelText,xlimRange, ylimRange, outputDir,outputName)
     figure('color','white','Units','centimeter','Position',[2,1,20,8])
+    colororder([0,0,0; 0,0,1])
+    yyaxis left
     plot(obsDateTime, obs,'k-');
     hold on
     plot(comDateTime, com,'r-')
     ylabel(ylabelText)
     xlim(xlimRange)
     ylim(ylimRange)
+    
+    yyaxis right
+    bar(obsDateTime, precip.*1800)
+    ylabel('Preci(mm/30min)')
     datetick('x','mm/dd')
     legend(legendText{:},'box','off')
     
--- src/plotSMresults.m
+++ src/plotSMresults.m
@@ -2,6 +2,7 @@
 % Sim_Theta = Sim_Theta(1:5808,:);
 % define directory
 S12Dir = '../../CH_HTC_2022/Observed/QMG_Hydro_S12.xlsx';
+% S12Dir = '../../CH_HTC_2022/Processed/Forcing_data_template_for_STEMMUS_SCOPE_CH-HTC_2022_0101_0809.xlsx';
 S12 = readtable(S12Dir);
 
 % select date period
@@ -39,9 +40,9 @@ f_drawSMST(simSMTable.dateTime, simSMTable.SM_10cm, S12.S12, S12.SM_10cm,{'Sim 1
 f_drawSMST(simSMTable.dateTime, simSMTable.SM_20cm, S12.S12, S12.SM_20cm,{'Sim 20cm','S12 20cm'}, 'Soil moisture(m^3/m^3)', [datetime(2022,1,1), datetime(2022,5,1)],[0.1,0.5],Output_dir,'SM_20cm')
 f_drawSMST(simSMTable.dateTime, simSMTable.SM_40cm, S12.S12, S12.SM_40cm,{'Sim 40cm','S12 40cm'}, 'Soil moisture(m^3/m^3)', [datetime(2022,1,1), datetime(2022,5,1)],[0.1,0.5],Output_dir,'SM_40cm')
 f_drawSMST(simSMTable.dateTime, simSMTable.SM_60cm, S12.S12, S12.SM_60cm,{'Sim 60cm','S12 60cm'}, 'Soil moisture(m^3/m^3)', [datetime(2022,1,1), datetime(2022,5,1)],[0.1,0.5],Output_dir,'SM_60cm')
-
-f_drawSMST(simSTTable.dateTime, simSTTable.ST_5cm,  S12.S12, S12.ST_0_5cm,{'Sim 5cm','S12 5cm'},  'Soil temperature(^\circC)', [datetime(2022,1,1), datetime(2022,5,1)],[0,40],Output_dir,'ST_5cm')
-f_drawSMST(simSTTable.dateTime, simSTTable.ST_10cm, S12.S12, S12.ST_10cm,{'Sim 10cm','S12 10cm'}, 'Soil temperature(^\circC)', [datetime(2022,1,1), datetime(2022,5,1)],[0,40],Output_dir,'ST_10cm')
-f_drawSMST(simSTTable.dateTime, simSTTable.ST_20cm, S12.S12, S12.ST_20cm,{'Sim 20cm','S12 20cm'}, 'Soil temperature(^\circC)', [datetime(2022,1,1), datetime(2022,5,1)],[0,40],Output_dir,'ST_20cm')
-f_drawSMST(simSTTable.dateTime, simSTTable.ST_40cm, S12.S12, S12.ST_40cm,{'Sim 40cm','S12 40cm'}, 'Soil temperature(^\circC)', [datetime(2022,1,1), datetime(2022,5,1)],[0,40],Output_dir,'ST_40cm')
-f_drawSMST(simSTTable.dateTime, simSTTable.ST_60cm, S12.S12, S12.ST_60cm,{'Sim 60cm','S12 60cm'}, 'Soil temperature(^\circC)', [datetime(2022,1,1), datetime(2022,5,1)],[0,40],Output_dir,'ST_60cm')
+%%
+% f_drawSMST(simSTTable.dateTime, simSTTable.ST_5cm,  S12.S12, S12.ST_0_5cm,{'Sim 5cm','S12 5cm'},  'Soil temperature(^\circC)', [datetime(2022,1,1), datetime(2022,5,1)],[0,40],Output_dir,'ST_5cm')
+% f_drawSMST(simSTTable.dateTime, simSTTable.ST_10cm, S12.S12, S12.ST_10cm,{'Sim 10cm','S12 10cm'}, 'Soil temperature(^\circC)', [datetime(2022,1,1), datetime(2022,5,1)],[0,40],Output_dir,'ST_10cm')
+% f_drawSMST(simSTTable.dateTime, simSTTable.ST_20cm, S12.S12, S12.ST_20cm,{'Sim 20cm','S12 20cm'}, 'Soil temperature(^\circC)', [datetime(2022,1,1), datetime(2022,5,1)],[0,40],Output_dir,'ST_20cm')
+% f_drawSMST(simSTTable.dateTime, simSTTable.ST_40cm, S12.S12, S12.ST_40cm,{'Sim 40cm','S12 40cm'}, 'Soil temperature(^\circC)', [datetime(2022,1,1), datetime(2022,5,1)],[0,40],Output_dir,'ST_40cm')
+% f_drawSMST(simSTTable.dateTime, simSTTable.ST_60cm, S12.S12, S12.ST_60cm,{'Sim 60cm','S12 60cm'}, 'Soil temperature(^\circC)', [datetime(2022,1,1), datetime(2022,5,1)],[0,40],Output_dir,'ST_60cm')
