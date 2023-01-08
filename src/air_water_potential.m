08cbe4791581de0f49dcc41df04cd57c8670dbf1
Z.Song <z.song@utwente.nl>
2023-01-08 23:52:56 +0100

    Debug: psiAir: RH is none unit rather than %.

--- src/air_water_potential.m
+++ src/air_water_potential.m
@@ -4,7 +4,8 @@ function [psiAir_m] = air_water_potential(RH, Ta)
 %     T:  air temperature (K)
 % output:
 %     psiAir: air water potential (m)
-%     
+%   
+    RH = RH./100;
     Ta = Ta+273.15; % unit transfer: Celsius degree to K
     R = 8.314; % gas constants (J/mol/K)
     Vw = 18;   % partial molar volume of liquid water (cm^3/mol)
