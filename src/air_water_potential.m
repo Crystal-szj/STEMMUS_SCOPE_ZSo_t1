3cfbf2a5742522e587db98705b9f000095260d4d
Z.Song <z.song@utwente.nl>
2023-03-03 17:33:39 +0100

    Add comments

--- src/air_water_potential.m
+++ src/air_water_potential.m
@@ -8,7 +8,7 @@ function [psiAir_m] = air_water_potential(RH, Ta)
     RH = RH./100;
     Ta = Ta+273.15; % unit transfer: Celsius degree to K
     R = 8.314; % gas constants (J/mol/K)
-    Vw = 18;   % partial molar volume of liquid water (cm^3/mol)
+    Vw = 18;   % partial molar volume of liquid water (cm^3/mol), J = N m = Pa m^3
     
     psiAir_MPa = R .* Ta ./ Vw .* log(RH); % unit [MPa]
     psiAir_m = psiAir_MPa .*1e6 ./9810;
