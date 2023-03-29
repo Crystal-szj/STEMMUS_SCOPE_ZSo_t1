d8937d210bb653372ebe4dc50c5c1371a20bb3b4
ZSo <z.song@utwente.nl>
2023-03-29 10:43:46 +0200

    Tuning G: Change gamma_s (0.95-->0.27) and QC (0.9-->0.2)

--- src/+equations/Soil_Inertia1.m
+++ src/+equations/Soil_Inertia1.m
@@ -12,7 +12,7 @@ theta_s = theta_s0; %(saturated water content, m3/m3)
 Sr = SMC/theta_s;
 
 %fss = 0.58; %(sand fraction)
-gamma_s = 0.95; %(A soil texture dependent parameter)
+gamma_s = 0.27; %(A soil texture dependent parameter)
                 % gamma_s = 0.95 for soils with sand fraction (fs) > 40%,
                 % and gamma_s = 0.27 for soils with fs <=40%
 dels = 1.33; %(shape parameter)
@@ -23,7 +23,7 @@ ke = exp(gamma_s*(1- power(Sr,(gamma_s - dels))));
 phis  = theta_s0; % phis is porosity (phis == theta_s); 
 lambda_d = -0.56*phis + 0.51;  % thermal conducticity for air-dry soil conditions
 
-QC = 0.90; %(quartz content)
+QC = 0.20; %(quartz content)
 lambda_qc = 7.7;  %(thermal conductivity of quartz, constant, unit: W m^-1 K^-1)
 
 lambda_s = (lambda_qc^(QC))*lambda_d^(1-QC);% thermal conductivity for saturated soil
